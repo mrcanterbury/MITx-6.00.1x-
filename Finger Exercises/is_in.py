19bcca2a7c2ecba46dfac24a466ccbbe2cd92a5f
mrcanterbury <mattrcanterbury@gmail.com>
2022-03-05 20:32:42 -0600

    initial commit

---
+++ Finger Exercises/is_in.py
@@ -0,0 +1,28 @@
+def isIn(char, aStr):
+    """
+    char: a single character
+    aStr: an alphabetized string
+
+    returns: True if char is in aStr; False otherwise
+    """
+
+    midIndex = len(aStr) // 2
+    midChar = aStr[midIndex]
+
+    if aStr == "":
+        return False
+
+    if len(aStr) == 1:
+        return aStr == char
+
+    if char == midChar:
+        return True
+
+    elif char < midChar:
+        return isIn(char, aStr[:midIndex])
+
+    else:
+        return isIn(char, aStr[midIndex + 1 :])
+
+
+print(isIn("p", "acddjlllpvwy"))
